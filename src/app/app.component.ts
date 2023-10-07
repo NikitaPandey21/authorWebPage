9348912bfe7b8e151765ed67a9b36c34759e788d
himal-kansagra <himal.kansagra@gmail.com>
2023-10-08 02:37:55 +0530

    Add Cover Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
     'NP1.jpg', 'NP2.jpg', 'NP3.jpg', 'NP4.jpg',
     'NP5.jpg', 'NP6.jpg', 'NP7.jpg', 'NP8.jpg'
   ];
+  coverImages = [1, 4, 5];
   activeIndex = 'img0';
 
   constructor(private modalService: NgbModal) { }
