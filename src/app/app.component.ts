90f00b948f1735551fab452083cd76b4aafc6803
himal-kansagra <himal.kansagra@gmail.com>
2023-10-08 00:04:48 +0530

    Portfolio Carousal Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,15 @@ export class AppComponent {
     'NP1.jpg', 'NP2.jpg', 'NP3.jpg', 'NP4.jpg',
     'NP5.jpg', 'NP6.jpg', 'NP7.jpg', 'NP8.jpg'
   ];
+  activeIndex = 'img0';
+
+  constructor(private modalService: NgbModal) { }
+
+  open(modal: any, index: number) {
+    this.activeIndex = 'img' + index;
+    this.modalService.open(modal, {
+      centered: true,
+      fullscreen: true
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbCarouselModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -9,6 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule,
+    NgbCarouselModule,
     AppRoutingModule
   ],
   providers: [],
